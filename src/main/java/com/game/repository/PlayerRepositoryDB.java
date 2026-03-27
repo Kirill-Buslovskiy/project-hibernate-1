cba3256b613717249c4341b636fb7bfd76b13790
Kirill Buslovskii <k.buslovsky@yandex.ru>
2026-03-28 00:46:21 +0300

    Feature imports

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -5,7 +5,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import org.hibernate.query.NativeQuery;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
@@ -15,18 +14,21 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
 
+import static org.hibernate.cfg.AvailableSettings.DRIVER;
+import static org.hibernate.cfg.Environment.*;
+
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
     private final SessionFactory sessionFactory;
 
     public PlayerRepositoryDB() {
         Properties properties = new Properties();
-        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
-        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3304/rpg");
-        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-        properties.put(Environment.USER, "root");
-        properties.put(Environment.PASS, "root");
-        properties.put(Environment.HBM2DDL_AUTO, "update");
+        properties.put(DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
+        properties.put(URL, "jdbc:p6spy:mysql://localhost:3304/rpg");
+        properties.put(DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(USER, "root");
+        properties.put(PASS, "root");
+        properties.put(HBM2DDL_AUTO, "update");
 
         sessionFactory = new Configuration()
                 .addAnnotatedClass(Player.class)
